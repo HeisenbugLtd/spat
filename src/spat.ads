cc825803f8784799217af3ca2c04d59d0615bb64
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-02-27 14:38:25 +0000

    * Initial commit, some code skeleton.

---
+++ src/spat.ads
@@ -0,0 +1,5 @@
+package SPAT with
+    Pure => True
+is
+
+end SPAT;
---
+++ test/main_spat_test.adb
@@ -0,0 +1,7 @@
+with Ada.Command_Line;
+with SPAT;
+
+procedure Main_SPAT_Test is
+begin
+  Ada.Command_Line.Set_Exit_Status (Code => Ada.Command_Line.Success);
+end Main_SPAT_Test;
