c9a08fc3692b331904f297703bc529c54c8c917f
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-02-29 17:31:38 +0100

    + Added some timing unit, making use of SI_Units.

--- src/run_spat.adb
+++ src/run_spat.adb
@@ -18,14 +18,26 @@ pragma License (Unrestricted);
 with Ada.Command_Line;
 with Ada.Directories;
 with Ada.IO_Exceptions;
+with Ada.Real_Time;
 with Ada.Text_IO;
 
+with SI_Units.Metric;
+with SI_Units.Names;
 with SPAT.File_Ops;
 with SPAT.Spark_Files;
 
 procedure Run_SPAT is
+
+   function Image is new
+     SI_Units.Metric.Fixed_Image (Item        => Duration,
+                                  Default_Aft => 3,
+                                  Unit        => SI_Units.Names.Second);
+
    File_List  : SPAT.File_Ops.File_List;
    SPARK_Data : SPAT.Spark_Files.SPARK_Data;
+   Start_Time : Ada.Real_Time.Time;
+
+   use type Ada.Real_Time.Time;
 begin
    --  TODO: Proper command line argument handling.
    if Ada.Command_Line.Argument_Count /= 1 then
@@ -38,9 +50,20 @@ begin
       declare
          Search_Dir : constant String := Ada.Command_Line.Argument (1);
       begin
+         Ada.Text_IO.Put ("Searching for "".spark"" files...");
+         Start_Time := Ada.Real_Time.Clock;
          File_List.Add_Files (Directory => Search_Dir,
                               Extension => "spark");
+         Ada.Text_IO.Put_Line
+           ("completed in " &
+              Image (Ada.Real_Time.To_Duration (Ada.Real_Time.Clock - Start_Time)));
+
+         Ada.Text_IO.Put ("Parsing "".spark"" files...");
+         Start_Time := Ada.Real_Time.Clock;
          SPARK_Data.Read_Files (Names => File_List);
+         Ada.Text_IO.Put_Line
+           ("completed in " &
+              Image (Ada.Real_Time.To_Duration (Ada.Real_Time.Clock - Start_Time)));
       exception
          when Ada.IO_Exceptions.Name_Error =>
             Ada.Text_IO.Put_Line
