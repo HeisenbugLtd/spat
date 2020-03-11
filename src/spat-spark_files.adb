b6b2e83ebf1a6446be20760964e7719d7e1c2204
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-03-11 08:18:51 +0000

    + Added license header.

--- src/spat-spark_files.adb
+++ src/spat-spark_files.adb
@@ -1,3 +1,12 @@
+------------------------------------------------------------------------------
+--  Copyright (C) 2020 by Heisenbug Ltd. (gh+spat@heisenbug.eu)
+--
+--  This work is free. You can redistribute it and/or modify it under the
+--  terms of the Do What The Fuck You Want To Public License, Version 2,
+--  as published by Sam Hocevar. See the LICENSE file for more details.
+------------------------------------------------------------------------------
+pragma License (Unrestricted);
+
 with Ada.IO_Exceptions;
 with Ada.Text_IO;
 with SPAT.Command_Line;
