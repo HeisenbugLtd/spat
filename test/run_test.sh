8329800a7b0331ce564bbffacf6e4d633695829d
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-06-07 15:00:08 +0200

    * Fixed test script.

--- test/run_test.sh
+++ test/run_test.sh
@@ -15,7 +15,7 @@ run_check () {
   for SPAT_OPTIONS in "-s -l -d -ca" "-s -l -d -ct"; do
     ../obj/run_spat -s -l -d -ct -P "$1/$2" > "spat.$1.$SPAT_OPTIONS.out"
     # Show template differences (FIXME: 'diff' might not be installed)
-    (diff -u "spat.$1.$SPAT_OPTIONS.template" "spat.$1.$SPAT_OPTIONS.out") >> test.diff || RESULT=2
+    (diff -u "spat.$1.$SPAT_OPTIONS.template" "spat.$1.$SPAT_OPTIONS.out") >> test.diff || RESULT=$?
   done
 }
 
@@ -31,17 +31,20 @@ RESULT=0
 run_check "test-saatana" "saatana.gpr"
 run_check "test-sparknacl" "src/sparknacl.gpr"
 
-if [ $RESULT -eq 0 ]; then
+case $RESULT in
+[0-1])
   if [ -s test.diff ]; then
     echo "Test failed, there are differences."
+    cat test.diff
     RESULT=1
   else
     echo "Test succeeded."
     RESULT=0
   fi
-else
+  ;;
+*)
   echo "One or more diffs failed."
-fi
+esac
 
 # Remove temp files
 rm -f test.diff *.out
