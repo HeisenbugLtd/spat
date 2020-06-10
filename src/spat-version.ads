b987f59359d279b1ee765a781965fd224a694424
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-06-10 22:26:02 +0200

    * Automated tagging of 0.9.0-prerelease version.

---
+++ src/spat-version.ads
@@ -0,0 +1,31 @@
+------------------------------------------------------------------------------
+--  Copyright (C) 2020 by Heisenbug Ltd. (gh+spat@heisenbug.eu)
+--
+--  This work is free. You can redistribute it and/or modify it under the
+--  terms of the Do What The Fuck You Want To Public License, Version 2,
+--  as published by Sam Hocevar. See the LICENSE file for more details.
+------------------------------------------------------------------------------
+pragma License (Unrestricted);
+
+------------------------------------------------------------------------------
+--
+--  SPARK Proof Analysis Tool
+--
+--  S.P.A.T. - Version information
+--
+------------------------------------------------------------------------------
+
+with GNAT.Compiler_Version;
+
+package SPAT.Version is
+
+   package Implementation is
+
+      package Compiler_Info is new GNAT.Compiler_Version;
+
+   end Implementation;
+
+   Number   : constant String := "0.9.0-prerelease";
+   Compiler : constant String := Implementation.Compiler_Info.Version;
+
+end SPAT.Version;
