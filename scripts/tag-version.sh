8c8b8e10d3dcf2a057d3feca7e8e80d14d648480
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-06-10 22:38:31 +0200

    * More safe guards.

--- scripts/tag-version.sh
+++ scripts/tag-version.sh
@@ -2,6 +2,8 @@
 
 # Change the version information in src/spat-version.ads
 
+# TODO: Sanity check of version.
+
 VFILE="src/spat-version.ads"
 
 sed 's/^\(\ \+Number\ \+:\ \+constant\ \+String\ \+:=\ \+\"\)\([0-9a-z.-]*\)\(\";\)/\1'$1'\3/' $VFILE > $VFILE.new || exit 1
@@ -15,10 +17,15 @@ mv $VFILE.new $VFILE
 # Recompile
 gprbuild -f -P spat.gpr || exit 3
 
-read -p "Last chance. If you really want me to commit and tag \"$1\", press ENTER, Ctrl-C to abort" || exit 255
+# TODO: Run executable and check that it reports the expected version.
+
+read -p "Last chance. If you really want me to commit and tag \"v$1\", press ENTER, Ctrl-C to abort" || exit 255
 
 # Compilation successful, add the file and commit, and then tag the version
-git commit $VFILE -m "* Automated tagging of $1 version." || exit 4
-git tag $1 || exit 5
+git commit --dry-run $VFILE -m "* Automated tagging of $1 version."
+read -p "Does the dry run look ok to you? Press ENTER to continue, Ctrl-C to abort" || exit 255
 
-echo "Version \"$1\" tagged. You may want to push upstream, though."
+git commit $VFILE -m "* Automated tagging of $1 version." || exit 4
+git tag v$1 || exit 5
+
+echo "Version \"v$1\" tagged. You may want to do 'git push --tags', though."
