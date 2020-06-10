93388f8768e23dc30b1c9f7ec6f3020cdf68f464
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-06-10 15:50:49 +0200

    * Niced up profiling script.

--- test/run_profiler.sh
+++ test/run_profiler.sh
@@ -4,7 +4,20 @@
 # Build and run a profiled executable.
 #
 
+SPAT_OPTIONS="-s -l -d -ct" # Summary, list, details, sort by time
+
+echo "Building instrumented executable..."
 gprbuild -f -P ../spat.gpr -cargs -pg -largs -pg || exit 1
-./run_test.sh
-gprof ../obj/run_spat gmon.out > profile-run.txt
+
+echo "Running..."
+
+# Run on test data 1
+../obj/run_spat $SPAT_OPTIONS -P test-saatana/saatana.gpr > /dev/null
+gprof ../obj/run_spat gmon.out > profile-run-1.txt
+
+# Run on test data 2
+../obj/run_spat $SPAT_OPTIONS -P test-sparknacl/src/sparknacl.gpr > /dev/null
+gprof ../obj/run_spat gmon.out > profile-run-2.txt
+
+echo "Rebuilding standard executable."
 gprbuild -f -P ../spat.gpr
