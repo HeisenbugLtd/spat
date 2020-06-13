6e71db26e2a1dea753f2b4245a39b95ab247bebd
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-06-13 12:42:23 +0200

    * Enhanced error reporting in tagging script.

--- scripts/tag-version.sh
+++ scripts/tag-version.sh
@@ -2,50 +2,60 @@
 
 # Change the version information in src/spat-version.ads
 
-# TODO: Sanity check of version.
-
-VERSION_NUMBER=$1
-
 if [ "$#" -ne 1 ]; then
     echo "No or more than one version number given. Please provide exactly one."
     exit 1
 fi
 
+VERSION_NUMBER=$1
+
+# 
+confirm()
+{
+    echo "$1"
+    read -p "Ctrl-C to abort, ENTER to continue..." || exit 255
+}
+
 # Validate input version number.
 
+# Check format
 if [ -z `echo $VERSION_NUMBER | grep -P '^(\d+\.)+(\d+\.)+(\d+)+([-][a-z]*)?$'` ]; then
     echo "Version number \"${VERSION_NUMBER}\" is not in format d.d.d[-suffix]"
-    exit 1
+    exit 2
 fi
 
+# Check if it's unique
 TAGS=`git tag | grep -P "^v${VERSION_NUMBER}$"`
 
 if [ -n "${TAGS}" ]; then
     echo "Version number \"${VERSION_NUMBER}\" has already been used."
-    exit 1
+    exit 3
 fi
 
+echo "Latest version number used was \"`git tag --sort=\"creatordate\" | tail -1`\", new one is \"v${VERSION_NUMBER}\"."
+confirm "Does that look fine to you?"
+
+# Patch file with new version number
 VFILE="src/spat-version.ads"
 
-sed 's/^\(\ \+Number\ \+:\ \+constant\ \+String\ \+:=\ \+\"\)\([0-9a-z.-]*\)\(\";\)/\1'${VERSION_NUMBER}'\3/' $VFILE > $VFILE.new || exit 1
-cat $VFILE.new || exit 2
+sed 's/^\(\ \+Number\ \+:\ \+constant\ \+String\ \+:=\ \+\"\)\([0-9a-z.-]*\)\(\";\)/\1'${VERSION_NUMBER}'\3/' $VFILE > $VFILE.new || (echo "Could not patch file. Are you in the root directory?"; exit 4)
 
-read -p "Does the file look ok? Press Ctrl-C to abort operation, ENTER to continue..." || exit 255
+cat $VFILE.new || (echo "Could not display file. That's odd."; exit 5)
+confirm "This would be the new version file."
 
 # Overwrite target file.
-mv $VFILE.new $VFILE
+mv $VFILE.new $VFILE || (echo "Failure to move file to final location!"; exit 6)
 
 # Recompile
-gprbuild -f -P spat.gpr || exit 3
+gprbuild -f -P spat.gpr || (echo "Compilation failed!"; exit 7)
 
 # TODO: Run executable and check that it reports the expected version.
 
-echo "Latest version number used was \"`git tag --sort=\"creatordate\" | tail -1`\", new one is \"v${VERSION_NUMBER}\"."
-read -p "Last chance. If you really want me to commit and tag \"v${VERSION_NUMBER}\", press ENTER, Ctrl-C to abort" || exit 255
+confirm "Last chance. Do you really want me to commit and tag \"v${VERSION_NUMBER}\"?"
 
 # Compilation successful, add the file and commit, and then tag the version
 git commit --dry-run $VFILE -m "* Automated tagging of ${VERSION_NUMBER} version."
-read -p "Does the dry run look ok to you? Press ENTER to continue, Ctrl-C to abort" || exit 255
+confirm "Does the dry run look ok to you?"
 
 git commit $VFILE -m "* Automated tagging of ${VERSION_NUMBER} version." || exit 4
 git tag v${VERSION_NUMBER} || exit 5
