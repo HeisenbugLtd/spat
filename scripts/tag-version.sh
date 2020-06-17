ba8156c8220cb44db01b2387f82940570d75b778
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-06-17 02:18:40 +0200

    Revert "* Automatically create signature from tar files."
    I have a different script for that now.

--- scripts/tag-version.sh
+++ scripts/tag-version.sh
@@ -7,8 +7,7 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-VERSION="$1"
-TAG="v${VERSION}"
+VERSION_NUMBER=$1
 
 # 
 confirm()
@@ -20,57 +19,45 @@ confirm()
 # Validate input version number.
 
 # Check format
-if [ -z `echo "${VERSION}" | grep -P '^(\d+\.)+(\d+\.)+(\d+)+([-][a-z0-9]+)?$'` ]; then
-    echo "Version number \"${VERSION}\" is not in format d.d.d[-suffix]"
+if [ -z `echo $VERSION_NUMBER | grep -P '^(\d+\.)+(\d+\.)+(\d+)+([-][a-z]*)?$'` ]; then
+    echo "Version number \"${VERSION_NUMBER}\" is not in format d.d.d[-suffix]"
     exit 2
 fi
 
 # Check if it's unique
-TAGS=`git tag | grep -P "^${TAG}$"`
+TAGS=`git tag | grep -P "^v${VERSION_NUMBER}$"`
 
 if [ -n "${TAGS}" ]; then
-    echo "Version number \"${VERSION}\" has already been used."
+    echo "Version number \"${VERSION_NUMBER}\" has already been used."
     exit 3
 fi
 
-echo "Latest version tag used was \"`git tag --sort=\"creatordate\" | tail -1`\", new one is \"${TAG}\"."
+echo "Latest version number used was \"`git tag --sort=\"creatordate\" | tail -1`\", new one is \"v${VERSION_NUMBER}\"."
 confirm "Does that look fine to you?"
 
 # Patch file with new version number
 VFILE="src/spat-version.ads"
 
-sed 's/^\(\ \+Number\ \+:\ \+constant\ \+String\ \+:=\ \+\"\)\([0-9a-z.-]*\)\(\";\)/\1'${VERSION}'\3/' "${VFILE}" > "${VFILE}.new" || (echo "Could not patch file. Are you in the root directory?"; exit 4)
+sed 's/^\(\ \+Number\ \+:\ \+constant\ \+String\ \+:=\ \+\"\)\([0-9a-z.-]*\)\(\";\)/\1'${VERSION_NUMBER}'\3/' $VFILE > $VFILE.new || (echo "Could not patch file. Are you in the root directory?"; exit 4)
 
-cat "${VFILE}.new" || (echo "Could not display file. That's odd."; exit 5)
+cat $VFILE.new || (echo "Could not display file. That's odd."; exit 5)
 confirm "This would be the new version file."
 
 # Overwrite target file.
-mv "${VFILE}.new" "${VFILE}" || (echo "Failure to move file to final location!"; exit 6)
+mv $VFILE.new $VFILE || (echo "Failure to move file to final location!"; exit 6)
 
 # Recompile
 gprbuild -f -P spat.gpr || (echo "Compilation failed!"; exit 7)
 
 # TODO: Run executable and check that it reports the expected version.
 
-confirm "Last chance. Do you really want me to commit and tag \"${TAG}\"?"
+confirm "Last chance. Do you really want me to commit and tag \"v${VERSION_NUMBER}\"?"
 
 # Compilation successful, add the file and commit, and then tag the version
-git commit --dry-run "${VFILE}" -m "* Automated tagging of ${VERSION} version."
+git commit --dry-run $VFILE -m "* Automated tagging of ${VERSION_NUMBER} version."
 confirm "Does the dry run look ok to you?"
 
-git commit "${VFILE}" -m "* Automated tagging of ${VERSION} version." || exit 4
-git tag "${TAG}" || exit 5
+git commit $VFILE -m "* Automated tagging of ${VERSION_NUMBER} version." || exit 4
+git tag v${VERSION_NUMBER} || exit 5
 
-echo "Version \"${VERSION}\" tagged as \"${TAG}\". You may want to do 'git push --tags', though."
-
-# Temporarily create the tarball and sign it.
-echo -n "Signing tar ball..."
-TMPDIR=`mktemp -d`
-TARFILE="${TMPDIR}/${TAG}.tar.gz"
-SIGFILE=`basename "${TARFILE}.asc"`
-
-git archive --prefix="${TAG}/" -o "${TARFILE}" "${TAG}"
-gpg --armor --detach-sign "${TARFILE}"
-mv "${TMPDIR}/${SIGFILE}" "./${SIGFILE}"
-rm -rf "${TMPDIR}"
-echo "signature file \"${SIGFILE}\" (hopefully) created."
+echo "Version \"v${VERSION_NUMBER}\" tagged. You may want to do 'git push --tags', though."
