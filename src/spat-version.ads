33e82efcff98eabd8dbcf020746329dbc4081193
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-06-19 02:02:19 +0200

    * Automated tagging of 0.9.5-pre-2 version.

--- src/spat-version.ads
+++ src/spat-version.ads
@@ -25,7 +25,7 @@ package SPAT.Version is
 
    end Implementation;
 
-   Number   : constant String := "0.9.5-pre-1";
+   Number   : constant String := "0.9.5-pre-2";
    Compiler : constant String := Implementation.Compiler_Info.Version;
 
 end SPAT.Version;
