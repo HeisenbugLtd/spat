aaa9788c61d7f943e89c726f688e9581f2e398a0
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-06-21 12:05:31 +0200

    * Automated tagging of 1.0.0-rc2 version.

--- src/spat-version.ads
+++ src/spat-version.ads
@@ -25,7 +25,7 @@ package SPAT.Version is
 
    end Implementation;
 
-   Number   : constant String := "1.0.0-rc1";
+   Number   : constant String := "1.0.0-rc2";
    Compiler : constant String := Implementation.Compiler_Info.Version;
 
 end SPAT.Version;
