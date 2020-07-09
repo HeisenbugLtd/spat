193d09bfd148caa721b5b692120f05d12ec80038
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-07-09 13:36:15 +0000

    Automated scaling of reported prover steps.

--- src/spat-proof_attempt.adb
+++ src/spat-proof_attempt.adb
@@ -46,6 +46,38 @@ package body SPAT.Proof_Attempt is
    is
       Time_Field : constant JSON_Value :=
                      Object.Get (Field => Field_Names.Time);
+
+      ------------------------------------------------------------------------
+      --  Scaled
+      --
+      --  Implement prover specific steps scaling.
+      ------------------------------------------------------------------------
+      function Scaled (Prover    : in Subject_Name;
+                       Raw_Steps : in Prover_Steps) return Prover_Steps;
+
+      ------------------------------------------------------------------------
+      --  Scaled
+      --
+      --  See https://github.com/AdaCore/why3/blob/master/src/gnat/gnat_config.ml#L538
+      ------------------------------------------------------------------------
+      function Scaled (Prover    : in Subject_Name;
+                       Raw_Steps : in Prover_Steps) return Prover_Steps is
+      begin
+         if Ada.Strings.Unbounded.Index (Source  => Prover,
+                                         Pattern => "CVC4") = 1
+         then
+            --  add = 15_000, mult = 35
+            return Prover_Steps'Max (Raw_Steps - 15_000, 0) / 35 + 1;
+         elsif Ada.Strings.Unbounded.Index (Source => Prover,
+                                            Pattern => "Z3") = 1
+         then
+            --  add = 450_000, mult = 800
+            return Prover_Steps'Max (Raw_Steps - 450_000, 0) / 800 + 1;
+         else
+            --  alt-ergo, and others => no scaling
+            return Raw_Steps + 1;
+         end if;
+      end Scaled;
    begin
       return T'(Entity.T with
                   Prover => Prover,
@@ -62,8 +94,11 @@ package body SPAT.Proof_Attempt is
                                "Fatal: Impossible Kind """ &
                                Time_Field.Kind'Image & """ of JSON object!"),
                 Steps =>
-                  Prover_Steps
-                    (Long_Integer'(Object.Get (Field => Field_Names.Steps))),
+                  Scaled (Prover    => Prover,
+                          Raw_Steps =>
+                            Prover_Steps
+                              (Long_Integer'
+                                 (Object.Get (Field => Field_Names.Steps)))),
                   Id    => Proof_Attempt_Ids.Next);
    end Create;
 
